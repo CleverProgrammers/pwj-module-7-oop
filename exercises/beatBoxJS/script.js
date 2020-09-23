925d8e0d2e01834649af420a29248c32263192fa
Steven Abaco <steven@Stevens-MacBook-Pro.local>
2020-09-22 22:11:24 -0500

    Initialize color and element values

--- exercises/beatBoxJS/script.js
+++ exercises/beatBoxJS/script.js
@@ -1,8 +1,8 @@
 /**
  * Done: Create a keydown listener to track what keys are hit
  * Done: Create a Beat class to represent the beat object in utils.js
- * TODO: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
- * TODO: Button pt1: Initialize color and element values
+ * Done: Complete triggerBeat() to play upon the press of a,s,d,f,g,h,j,k,l
+ * Done: Button pt1: Initialize color and element values
  * TODO: Button pt2: Set button color upon initialization | Initialize button in beats["65"]
  * TODO: Button pt3: Complete select function to set the color and shadow of button upon pressing
  * TODO: Button pt4: Call the select() function upon key press ;)
@@ -18,6 +18,7 @@
 let beats = {
     "65": {
         beat: new Beat("./assets/Piano Chord 331.mp3"),
+        button: new Button('#00fffe', 65)
     },
     "83": {
         beat: new Beat("./assets/Piano Chord 209.mp3"),
@@ -50,6 +51,11 @@ let beats = {
  * HINT: use the keyCode
  */
 triggerBeat = (event) => {
+ const keyCode = event.keyCode;
+ if(keyCode in beats){
+    let keyPress = beats[keyCode];
+    keyPress.beat.play()
+ }
 
 }
 
@@ -58,6 +64,4 @@ triggerBeat = (event) => {
  * HINT: Log the keyCode of the key
  */
 
-document.addEventListener('keydown', (event)=>{
-console.log(event.code);
-})
+document.addEventListener('keydown', triggerBeat)
--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -5,11 +5,11 @@
  */
 class Beat {
         constructor(audioSrc){
-            this.audio = new Audio(audioSrc);
-            console.log(this.audio);
-        }
+            this.audio = new Audio(audioSrc);     
+          
+        } 
 
-        play = ()=> {
+        play = () => {
             this.audio.currentTime = 0;
             this.audio.play();
         }
@@ -23,6 +23,10 @@ class Beat {
 
  class Button {
     constructor(color, keyCode){
+        this.color = color;
+        this.keyCode = keyCode;
+        this.element = document.getElementById(keyCode);
+        console.log(this.element)
     }
 
     /**
