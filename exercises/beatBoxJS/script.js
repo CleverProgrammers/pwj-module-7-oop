8835c54d2dab85e6e3f9693c2724a876be461c9d
Steven Abaco <steven@Stevens-MacBook-Pro.local>
2020-09-24 22:48:54 -0500

    Add all new buttons to beats

--- exercises/beatBoxJS/script.js
+++ exercises/beatBoxJS/script.js
@@ -8,7 +8,7 @@
  * Done: Button pt4: Call the select() function upon key press ;)
  * Done: Button pt5: Add transition for button selection
  * Done: Button pt6: Remove the button style upon transition end | Use deselect function
- * TODO: Complete all button instances with the following colors
+ * Done: Complete all button instances with the following colors
  * TODO: Add background image
  * First 3: #00fffe
  * 4,5,6,7: #FF00FF
@@ -16,54 +16,60 @@
  */
 
 let beats = {
-    "65": {
-        beat: new Beat("./assets/Piano Chord 331.mp3"),
-        button: new Button('#00fffe', 65)
-    },
-    "83": {
-        beat: new Beat("./assets/Piano Chord 209.mp3"),
-    },
-    "68": {
-        beat: new Beat("./assets/Piano Chord 208.mp3"),
-    },
-    "70": {
-        beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
-    },
-    "71": {
-        beat: new Beat("./assets/Drum Snare Roll.mp3"),
-    },
-    "72": {
-        beat: new Beat("./assets/PREL Musical 57.mp3"),
-    },
-    "74": {
-        beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
-    },
-    "75": {
-        beat: new Beat("./assets/Musical Compos 33.mp3"),
-    },
-    "76": {
-        beat: new Beat("./assets/Musical Orches 4.mp3"),
-    }
-}
+	65: {
+		beat: new Beat("./assets/Piano Chord 331.mp3"),
+		button: new Button("#00fffe", 65),
+	},
+	83: {
+		beat: new Beat("./assets/Piano Chord 209.mp3"),
+		button: new Button("#00fffe", 83),
+	},
+	68: {
+		beat: new Beat("./assets/Piano Chord 208.mp3"),
+		button: new Button("#00fffe", 68),
+	},
+	70: {
+		beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+		button: new Button("#FF00FF", 70),
+	},
+	71: {
+		beat: new Beat("./assets/Drum Snare Roll.mp3"),
+		button: new Button("#FF00FF", 71),
+	},
+	72: {
+		beat: new Beat("./assets/PREL Musical 57.mp3"),
+		button: new Button("#FF00FF", 72),
+	},
+	74: {
+		beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+		button: new Button("#FF00FF", 74),
+	},
+	75: {
+		beat: new Beat("./assets/Musical Compos 33.mp3"),
+		button: new Button("#FFFFFF", 75),
+	},
+	76: {
+		beat: new Beat("./assets/Musical Orches 4.mp3"),
+		button: new Button("#FFFFFF", 76),
+	},
+};
 
 /**
  * Function to play the beat upon a press of key
  * HINT: use the keyCode
  */
 triggerBeat = (event) => {
- const keyCode = event.keyCode;
- if(keyCode in beats){
-    let keyPress = beats[keyCode];
-    keyPress.beat.play();
-    keyPress.button.select();
-    
- }
-
-}
+	const keyCode = event.keyCode;
+	if (keyCode in beats) {
+		let keyPress = beats[keyCode];
+		keyPress.beat.play();
+		keyPress.button.select();
+	}
+};
 
 /**
  * Keydown listener to fire triggerBeat function
  * HINT: Log the keyCode of the key
  */
 
-document.addEventListener('keydown', triggerBeat)
+document.addEventListener("keydown", triggerBeat);
